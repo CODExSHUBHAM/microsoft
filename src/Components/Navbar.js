aa99db2d325f2542e11334181aa2be191d0c960d
CODExSHUBHAM <Shubham.nh10@gmail.com>
2022-07-28 20:59:40 +0530

    Feat: updated navbar components expanded view

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,52 +1,75 @@
 import React from "react";
 import logo from "../Assets/logo.png";
 import account from "../Assets/account.svg";
+import search from "../Assets/search-icon.png"
+import cart from "../Assets/cart-icon.png"
 
 function Navbar() {
   return (
-    <div className="navbar flex justify-between items-center p-2">
-      <div className="flex items-center lg:hidden">
-        <div className="hamburger p-2 inline-block cursor-pointer">
-          <div className="line h-0.5 w-5 my-1 bg-gray-500"></div>
-          <div className="line h-0.5 w-5 my-1 bg-gray-500"></div>
-          <div className="line h-0.5 w-5 my-1 bg-gray-500"></div>
+    <>
+      <div className="navbar flex justify-between items-center p-2">
+        <div className="flex items-center lg:hidden">
+          <div className="hamburger p-2 inline-block cursor-pointer">
+            <div className="line h-0.5 w-5 my-1 bg-gray-500"></div>
+            <div className="line h-0.5 w-5 my-1 bg-gray-500"></div>
+            <div className="line h-0.5 w-5 my-1 bg-gray-500"></div>
+          </div>
+          <div className="search"><img src={search} alt="search-button" className="w-8 ml-2" /></div>
         </div>
-        <div className="search">search</div>
+
+        <div className="logo-navlist flex items-center">
+          <img src={logo} alt="microsoft-logo" className="w-28 mx-2" />
+          <ul className="hidden lg:flex items-center">
+            <li className="mx-2">
+              <a href="/">Microsoft 365</a>
+            </li>
+            <li className="mx-2">
+              <a href="/">Office</a>
+            </li>
+            <li className="mx-2">
+              <a href="/">Windows</a>
+            </li>
+            <li className="mx-2">
+              <a href="/">Surface</a>
+            </li>
+            <li className="mx-2">
+              <a href="/">Xbox</a>
+            </li>
+            <li className="mx-2">
+              <a href="/">Support</a>
+            </li>
+          </ul>
+        </div>
+
+        <div className="flex items-center">
+          <div className=" hidden lg:block mx-2">All Microsoft</div>
+          <div className="search hidden lg:block mx-2"><div className="search"><img src={search} alt="search-button" className="w-8 ml-2" /></div></div>
+          <div className="cart mx-2"><img src={cart} alt="cart-button" className="w-4 mr-2" /></div>
+          <div className="account mx-2">
+            <img src={account} alt="account-button" className="w-8" />
+          </div>
+        </div>
       </div>
 
-      <div className="logo-navlist flex items-center">
-        <img src={logo} alt="microsoft-logo" className="w-28 mx-2" />
-        <ul className="hidden lg:flex items-center">
-          <li className="mx-2">
-            <a href="/">Microsoft 365</a>
-          </li>
-          <li className="mx-2">
-            <a href="/">Office</a>
-          </li>
-          <li className="mx-2">
-            <a href="/">Windows</a>
-          </li>
-          <li className="mx-2">
-            <a href="/">Surface</a>
-          </li>
-          <li className="mx-2">
-            <a href="/">Xbox</a>
-          </li>
-          <li className="mx-2">
-            <a href="/">Support</a>
-          </li>
+      <div className="mobile-nav bg-gray-100 text-md hidden">
+        <ul className="">
+          <li className="p-4 border-b border-[#e6e6e6]"><a href="/">Microsoft 365</a></li>
+          <li className="p-4 border-b border-[#e6e6e6]"><a href="/">Office</a></li>
+          <li className="p-4 border-b border-[#e6e6e6]"><a href="/">Windows</a></li>
+          <li className="p-4 border-b border-[#e6e6e6]"><a href="/">Surface</a></li>
+          <li className="p-4 border-b border-[#e6e6e6]"><a href="/">Xbox</a></li>
+          <li className="p-4 border-b border-[#e6e6e6]"><a href="/">Support</a></li>
+          <li className="p-4 border-b border-[#e6e6e6]"><a href="/">Software</a></li>
+          <li className="p-4 border-b border-[#e6e6e6]"><a href="/">PCs & Devices</a></li>
+          <li className="p-4 border-b border-[#e6e6e6]"><a href="/">Entertainment</a></li>
+          <li className="p-4 border-b border-[#e6e6e6]"><a href="/">Business</a></li>
+          <li className="p-4 border-b border-[#e6e6e6]"><a href="/">Developer & IT</a></li>
+          <li className="p-4 border-b border-[#e6e6e6]"><a href="/">Other</a></li>
+          <li className="p-4 border-b border-black"><a href="/">View Sitemap</a></li>
         </ul>
       </div>
 
-      <div className="flex items-center">
-        <div className=" hidden lg:block mx-2">All Microsoft</div>
-        <div className="search hidden lg:block mx-2">search</div>
-        <div className="cart mx-2">cart</div>
-        <div className="account mx-2">
-          <img src={account} alt="account-button" className="w-8" />
-        </div>
-      </div>
-    </div>
+    </>
   );
 }
 
