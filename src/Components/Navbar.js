8ffd7c27277c0d89f6f8fca847ecf77d036dfb45
CODExSHUBHAM <Shubham.nh10@gmail.com>
2022-07-28 21:43:28 +0530

    Feat: added open-close functionality to mobile navbar

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,18 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
 import logo from "../Assets/logo.png";
 import account from "../Assets/account.svg";
 import search from "../Assets/search-icon.png"
 import cart from "../Assets/cart-icon.png"
+import close from "../Assets/close.png"
+
 
 function Navbar() {
+
+  const [isOpen, setIsOpen] = useState(false)
+
+  const openNavbar = () => {
+    if (!isOpen) {
+      setIsOpen(true)
+    }
+    else {
+      setIsOpen(false)
+    }
+  }
+
   return (
+
     <>
+
       <div className="navbar flex justify-between items-center p-2">
+
         <div className="flex items-center lg:hidden">
-          <div className="hamburger p-2 inline-block cursor-pointer">
-            <div className="line h-0.5 w-5 my-1 bg-gray-500"></div>
-            <div className="line h-0.5 w-5 my-1 bg-gray-500"></div>
-            <div className="line h-0.5 w-5 my-1 bg-gray-500"></div>
+          <div className="hamburger p-2 inline-block cursor-pointer" onClick={openNavbar}>
+            <div className={`${isOpen ? 'hidden' : 'block'}`}>
+              <div className="line h-0.5 w-5 my-1 bg-gray-500"></div>
+              <div className="line h-0.5 w-5 my-1 bg-gray-500"></div>
+              <div className="line h-0.5 w-5 my-1 bg-gray-500"></div>
+            </div>
+            <div>
+              <img src={close} alt="close-button" className={`w-4 ml-2 ${isOpen ? 'block' : 'hidden'} `} />
+            </div>
           </div>
           <div className="search"><img src={search} alt="search-button" className="w-8 ml-2" /></div>
         </div>
@@ -51,7 +73,7 @@ function Navbar() {
         </div>
       </div>
 
-      <div className="mobile-nav bg-gray-100 text-md hidden">
+      <div className={`mobile-nav bg-gray-100 text-md ${isOpen ? 'block' : 'hidden'}`}>
         <ul className="">
           <li className="p-4 border-b border-[#e6e6e6]"><a href="/">Microsoft 365</a></li>
           <li className="p-4 border-b border-[#e6e6e6]"><a href="/">Office</a></li>
